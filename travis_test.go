a590c2b1bf63b12ad7a104765160a2e9cb1c6dfd
tcr <tcr@ableton.com>
2015-06-29 12:13:11 +0200

    Initial commit

---
+++ travis_test.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestClient_NewRequest(t *testing.T) {
+	c := NewClient(TRAVIS_API_DEFAULT_URL, "")
+
+	req, err := c.NewRequest("GET", "/test", nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert(
+		t,
+		req.Method == "GET",
+		"Wrong Request Method set",
+	)
+
+	assert(
+		t,
+		req.URL.String() == TRAVIS_API_DEFAULT_URL+"test",
+		"Wrong Request URL set",
+	)
+
+}
+
+func TestClient_NewRequest_with_nil_headers_provided(t *testing.T) {
+	baseUrl, _ := url.Parse(TRAVIS_API_DEFAULT_URL)
+	c := NewClient(TRAVIS_API_DEFAULT_URL, "")
+
+	req, err := c.NewRequest("GET", "/users", nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert(
+		t,
+		req.Header.Get("User-Agent") == TRAVIS_USER_AGENT,
+		"Wrong Request User-Agent header set",
+	)
+
+	assert(
+		t,
+		req.Header.Get("Accept") == TRAVIS_REQUEST_ACCEPT_HEADER,
+		"Wrong Request Accept header set",
+	)
+
+	assert(
+		t,
+		req.Header.Get("Host") == baseUrl.Host,
+		"Wrong Request Host header set",
+	)
+}
+
+func TestClient_NewRequest_with_non_overriding_headers_provided(t *testing.T) {
+	baseUrl, _ := url.Parse(TRAVIS_API_DEFAULT_URL)
+	c := NewClient(TRAVIS_API_DEFAULT_URL, "")
+	h := map[string]string{
+		"Abc": "123",
+	}
+
+	req, err := c.NewRequest("GET", "/users", nil, h)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert(
+		t,
+		req.Header.Get("Abc") == "123",
+		"Wrong Request Abc header set",
+	)
+
+	assert(
+		t,
+		req.Header.Get("User-Agent") == TRAVIS_USER_AGENT,
+		"Wrong Request User-Agent header set",
+	)
+
+	assert(
+		t,
+		req.Header.Get("Accept") == TRAVIS_REQUEST_ACCEPT_HEADER,
+		"Wrong Request Accept header set",
+	)
+
+	assert(
+		t,
+		req.Header.Get("Host") == baseUrl.Host,
+		"Wrong Request Host header set",
+	)
+}
+
+func TestClient_NewRequest_with_overriding_headers_provided(t *testing.T) {
+	c := NewClient(TRAVIS_API_DEFAULT_URL, "")
+	h := map[string]string{
+		"Host": "api.travis-ci.com",
+	}
+
+	req, err := c.NewRequest("GET", "/users", nil, h)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert(
+		t,
+		req.Header.Get("User-Agent") == TRAVIS_USER_AGENT,
+		"Wrong Request User-Agent header set",
+	)
+
+	assert(
+		t,
+		req.Header.Get("Accept") == TRAVIS_REQUEST_ACCEPT_HEADER,
+		"Wrong Request Accept header set",
+	)
+
+	assert(
+		t,
+		req.Header.Get("Host") == "api.travis-ci.com",
+		"Wrong Request Host header set",
+	)
+}
