746626a7f5061ecfadafd0aae7227eb67f1e54f9
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-08-25 19:51:59 +0900

    Refactor init integration test

--- init_integration_test.go
+++ init_integration_test.go
@@ -3,30 +3,21 @@
 package travis
 
 import (
-	"fmt"
 	"os"
 )
 
 var (
-	integrationClient *Client
-	integrationToken  string
-	integrationUrl    string = defaultBaseURL
-	integrationRepo   string = "Ableton/go-travis"
+	integrationClient      *Client
+	integrationToken       string = os.Getenv("TRAVIS_API_AUTH_TOKEN")
+	integrationGitHubToken string = os.Getenv("TRAVIS_GITHUB_PERSONAL_ACCESS_TOKEN")
+	integrationUrl         string = defaultBaseURL
+	integrationRepo        string = "shuheiktgw/go-travis"
 )
 
 func init() {
 	url := os.Getenv("TRAVIS_API_URL")
 	if url != "" {
 		integrationUrl = url
-	}
-
-	integrationToken := os.Getenv("TRAVIS_API_AUTH_TOKEN")
-	if integrationToken == "" {
-		fmt.Println(
-			"TRAVIS_API_AUTH_TOKEN environment variable not set. ",
-			"Unable to authenticate the integration tests client. ",
-			"Some tests won't run!",
-		)
 	}
 
 	integrationClient = NewClient(integrationUrl, integrationToken)
