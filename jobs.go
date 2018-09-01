0ddb8f2ec6093b516acf743759680471ee282aa6
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-09-01 11:14:19 +0900

    Add support for FindByBuild method

--- jobs.go
+++ jobs.go
@@ -1,12 +1,9 @@
 package travis
 
 import (
+	"context"
 	"fmt"
 	"net/http"
-
-	"context"
-
-	"github.com/fatih/structs"
 )
 
 // JobsService handles communication with the jobs
@@ -15,58 +12,15 @@ type JobsService struct {
 	client *Client
 }
 
-type findJobsResponse struct {
+type getJobsResponse struct {
 	Jobs []Job `json:"jobs"`
 }
 
-// getJobResponse represents the response of a call
-// to the Travis CI get build endpoint.
-type getJobResponse struct {
-	Job Job `json:"job"`
-}
-
-// JobListOptions specifies the optional parameters to the
-// JobsService.List method. You need to provide exactly one
-// of the below attribute. If you provide State or Queue, a
-// maximum of 250 jobs will be returned.
-type JobFindOptions struct {
-	ListOptions
-
-	// List of job ids
-	Ids []uint `url:"ids,omitempty"`
-
-	// Job state to filter by
-	State string `url:"state,omitempty"`
-
-	// Job queue to filter by
-	Queue string `url:"queue,omitempty"`
-}
-
-// IsValid asserts the JobFindOptions instance has one
-// and only one value set to a non-zero value.
+// FindByBuild fetches a jobs based on the provided build id
 //
-// This method is particularly useful to check a JobFindOptions
-// instance before passing it to JobsService.FindByID method.
-func (jfo *JobFindOptions) IsValid() bool {
-	s := structs.New(jfo)
-	f := s.Fields()
-
-	nonZeroValues := 0
-
-	for _, field := range f {
-		if !field.IsZero() {
-			nonZeroValues += 1
-		}
-	}
-
-	return nonZeroValues == 0 || nonZeroValues == 1
-}
-
-// Get fetches job with the provided id.
-//
-// Travis CI API docs: http://docs.travis-ci.com/api/#jobs
-func (js *JobsService) Get(ctx context.Context, id uint) (*Job, *http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/jobs/%d", id), nil)
+// Travis CI API docs: https://developer.travis-ci.com/resource/jobs#find
+func (js *JobsService) FindByBuild(ctx context.Context, buildId uint) ([]Job, *http.Response, error) {
+	u, err := urlWithOptions(fmt.Sprintf("/build/%d/jobs", buildId), nil)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -76,109 +30,11 @@ func (js *JobsService) Get(ctx context.Context, id uint) (*Job, *http.Response, 
 		return nil, nil, err
 	}
 
-	var jobResp getJobResponse
-	resp, err := js.client.Do(ctx, req, &jobResp)
+	var getJobsResponse getJobsResponse
+	resp, err := js.client.Do(ctx, req, &getJobsResponse)
 	if err != nil {
 		return nil, resp, err
 	}
 
-	return &jobResp.Job, resp, err
-}
-
-// ListByBuild retrieve a build jobs from its provided id.
-//
-// Travis CI API docs: http://docs.travis-ci.com/api/#jobs
-func (js *JobsService) ListFromBuild(ctx context.Context, buildId uint) ([]Job, *http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/builds/%d", buildId), nil)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	req, err := js.client.NewRequest("GET", u, nil, nil)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	var buildResp getBuildResponse
-	resp, err := js.client.Do(ctx, req, &buildResp)
-	if err != nil {
-		return nil, resp, err
-	}
-
-	return buildResp.Jobs, resp, err
-}
-
-// FindByID jobs using the provided options.
-// You need to provide exactly one of the opt fields value.
-// If you provide State or Queue, a maximum of 250 jobs will be returned.
-//
-// Travis CI API docs: http://docs.travis-ci.com/api/#jobs
-func (js *JobsService) Find(ctx context.Context, opt *JobFindOptions) ([]Job, *http.Response, error) {
-	if opt != nil && !opt.IsValid() {
-		return nil, nil, fmt.Errorf(
-			"more than one value set in provided JobFindOptions instance",
-		)
-	}
-
-	u, err := urlWithOptions("/jobs", opt)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	req, err := js.client.NewRequest("GET", u, nil, nil)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	var jobsResp findJobsResponse
-	resp, err := js.client.Do(ctx, req, &jobsResp)
-	if err != nil {
-		return nil, resp, err
-	}
-
-	return jobsResp.Jobs, resp, err
-}
-
-// Cancel job with the provided id.
-//
-// Travis CI API docs: http://docs.travis-ci.com/api/#jobs
-func (js *JobsService) Cancel(ctx context.Context, id uint) (*http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/jobs/%d/cancel", id), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := js.client.NewRequest("POST", u, nil, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := js.client.Do(ctx, req, nil)
-	if err != nil {
-		return resp, err
-	}
-
-	return resp, err
-}
-
-// Restart job with the provided id.
-//
-// Travis CI API docs: http://docs.travis-ci.com/api/#jobs
-func (js *JobsService) Restart(ctx context.Context, id uint) (*http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/jobs/%d/restart", id), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := js.client.NewRequest("POST", u, nil, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := js.client.Do(ctx, req, nil)
-	if err != nil {
-		return resp, err
-	}
-
-	return resp, err
+	return getJobsResponse.Jobs, resp, err
 }
--- jobs_integration_test.go
+++ jobs_integration_test.go
@@ -4,97 +4,24 @@ package travis
 
 import (
 	"context"
+	"net/http"
 	"testing"
 )
 
-func TestJobsService_Find_without_options(t *testing.T) {
-	_, _, err := integrationClient.Jobs.Find(context.TODO(), nil)
-	ok(t, err) // As jobs could be nil, that's unfortunately the only assertion we can make
-}
+const buildId = 420907933
 
-func TestJobsService_Find_with_options(t *testing.T) {
-	// Make sure to fetch jobs, and extract first returned
-	// job queue to be able to filter against an existing one
-	// later on
-	unfilteredJobs, _, err := integrationClient.Jobs.Find(context.TODO(), nil)
-	jobQueue := unfilteredJobs[0].Queue
+func TestJobsService_FindByBuild(t *testing.T) {
+	jobs, res, err := integrationClient.Jobs.FindByBuild(context.TODO(), buildId)
 
-	opt := &JobFindOptions{Queue: jobQueue}
-	jobs, _, err := integrationClient.Jobs.Find(context.TODO(), opt)
-	ok(t, err)
-
-	if jobs != nil {
-		for _, j := range jobs {
-			assert(
-				t,
-				j.Queue == jobQueue,
-				"JobsService.FindByID return a job with Queue %s; expected %s", j.Queue, jobQueue,
-			)
-		}
+	if err != nil {
+		t.Fatalf("unexpected error occured: %s", err)
 	}
-}
 
-func TestJobsService_ListFromBuild(t *testing.T) {
-	// Fetch an existing build id
-	builds, _, _, _, err := integrationClient.Builds.List(context.TODO(), nil)
-	buildId := builds[0].Id
-
-	jobs, _, err := integrationClient.Jobs.ListFromBuild(context.TODO(), buildId)
-	ok(t, err)
-
-	if jobs != nil {
-		for _, j := range jobs {
-			assert(
-				t,
-				j.Build.Id == buildId,
-				"JobsService.ListFromBuild return a job with BuildId %d; expected %d", j.Build.Id, buildId,
-			)
-		}
+	if res.StatusCode != http.StatusOK {
+		t.Fatalf("#invalid http status: %s", res.Status)
 	}
-}
 
-func TestJobsService_Get(t *testing.T) {
-	jobs, _, err := integrationClient.Jobs.Find(context.TODO(), nil)
-	if jobs == nil || len(jobs) == 0 {
-		t.Skip("No jobs found for the provided integration repo. skipping test")
+	if len(jobs) == 0 {
+		t.Fatalf("returned empty jobs")
 	}
-	jobId := jobs[0].Id
-
-	job, _, err := integrationClient.Jobs.Get(context.TODO(), jobId)
-	ok(t, err)
-
-	assert(
-		t,
-		job != nil,
-		"JobsService.Get returned a nil job",
-	)
-
-	assert(
-		t,
-		job.Id == jobId,
-		"JobsService.Get returned job with id %d; expected %d",
-	)
-}
-
-func TestJobFindOptions_IsValid(t *testing.T) {
-	opt := &JobFindOptions{}
-	assert(
-		t,
-		opt.IsValid() == true,
-		"JobFindOptions.IsValid returned %t; expected %t", opt.IsValid(), true,
-	)
-
-	opt = &JobFindOptions{Queue: "test"}
-	assert(
-		t,
-		opt.IsValid() == true,
-		"JobFindOptions.IsValid returned %t; expected %t", opt.IsValid(), true,
-	)
-
-	opt = &JobFindOptions{Queue: "test", State: "a state"}
-	assert(
-		t,
-		opt.IsValid() == false,
-		"JobFindOptions.IsValid returned %t; expected %t", opt.IsValid(), false,
-	)
 }
