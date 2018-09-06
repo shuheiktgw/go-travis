e0e35aaaa1ea3da6f4af0001907f97bfd44174f9
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-09-07 08:45:41 +0900

    Add support for branches endpoints

--- branches.go
+++ branches.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 // BranchesService handles communication with the branches
@@ -12,23 +13,26 @@ type BranchesService struct {
 	client *Client
 }
 
-// listBranchesResponse represents the response of a call
-// to the Travis CI list branches endpoint.
-type listBranchesResponse struct {
+// BranchesOption specifies the optional parameters for branches endpoint
+type BranchesOption struct {
+	ExistOnGithub bool   `url:"exists_on_github,omitempty"`
+	Limit         int    `url:"limit,omitempty"`
+	Offset        int    `url:"offset,omitempty"`
+	SortBy        string `url:"sort_by,omitempty"`
+}
+
+// getBranchesResponse represents the response of a call
+// to the Travis CI branches endpoint.
+type getBranchesResponse struct {
 	Branches []Branch `json:"branches"`
 }
 
-// getBranchResponse represents the response of a call
-// to the Travis CI get branch endpoint.
-type getBranchResponse struct {
-	Branch *Branch `json:"branch"`
-}
-
-// List the branches of a given repository.
+// FindByRepositoryId fetches the branches of a given repository id.
+// As of 2018/9/7 this endpoints returns 404 and does not seem to work correctly
 //
-// Travis CI API docs: http://docs.travis-ci.com/api/#builds
-func (bs *BranchesService) ListFromRepository(ctx context.Context, slug string) ([]Branch, *http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/repos/%v/branches", slug), nil)
+// Travis CI API docs: https://developer.travis-ci.com/resource/branches#find
+func (bs *BranchesService) FindByRepositoryId(ctx context.Context, repositoryId uint, opt *BranchesOption) ([]Branch, *http.Response, error) {
+	u, err := urlWithOptions(fmt.Sprintf("/repos/%d/branches", repositoryId), opt)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -38,21 +42,21 @@ func (bs *BranchesService) ListFromRepository(ctx context.Context, slug string) 
 		return nil, nil, err
 	}
 
-	var branchesResp listBranchesResponse
-	resp, err := bs.client.Do(ctx, req, &branchesResp)
+	var getBranchesResponse getBranchesResponse
+	resp, err := bs.client.Do(ctx, req, &getBranchesResponse)
 	if err != nil {
 		return nil, resp, err
 	}
 
-	return branchesResp.Branches, resp, err
+	return getBranchesResponse.Branches, resp, err
 }
 
-// Get fetches a branch based on the provided repository slug
-// and it's id.
+// FindByRepositorySlug fetches the branches of a given repository slug.
+// As of 2018/9/7 this endpoints returns 404 and does not seem to work correctly
 //
-// Travis CI API docs: http://docs.travis-ci.com/api/#builds
-func (bs *BranchesService) Get(ctx context.Context, repoSlug string, branchId uint) (*Branch, *http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/repos/%v/branches/%d", repoSlug, branchId), nil)
+// Travis CI API docs: https://developer.travis-ci.com/resource/branches#find
+func (bs *BranchesService) FindByRepositorySlug(ctx context.Context, repositorySlug string, opt *BranchesOption) ([]Branch, *http.Response, error) {
+	u, err := urlWithOptions(fmt.Sprintf("/repos/%s/branches", url.QueryEscape(repositorySlug)), opt)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -62,35 +66,11 @@ func (bs *BranchesService) Get(ctx context.Context, repoSlug string, branchId ui
 		return nil, nil, err
 	}
 
-	var branchResp getBranchResponse
-	resp, err := bs.client.Do(ctx, req, &branchResp)
+	var getBranchesResponse getBranchesResponse
+	resp, err := bs.client.Do(ctx, req, &getBranchesResponse)
 	if err != nil {
 		return nil, resp, err
 	}
 
-	return branchResp.Branch, resp, err
-}
-
-// Get fetches a branch based on the provided repository slug
-// and its name.
-//
-// Travis CI API docs: http://docs.travis-ci.com/api/#builds
-func (bs *BranchesService) GetFromSlug(ctx context.Context, repoSlug string, branchSlug string) (*Branch, *http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/repos/%v/branches/%v", repoSlug, branchSlug), nil)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	req, err := bs.client.NewRequest("GET", u, nil, nil)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	var branchResp getBranchResponse
-	resp, err := bs.client.Do(ctx, req, &branchResp)
-	if err != nil {
-		return nil, resp, err
-	}
-
-	return branchResp.Branch, resp, err
+	return getBranchesResponse.Branches, resp, err
 }
--- branches_integration_test.go
+++ branches_integration_test.go
@@ -4,12 +4,69 @@ package travis
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"testing"
 )
 
-func TestBranchesService_ListFromRepository(t *testing.T) {
+func TestBranchesService_FindByRepositoryId(t *testing.T) {
 	t.Parallel()
 
-	_, _, err := integrationClient.Branches.ListFromRepository(context.TODO(), integrationRepo)
-	ok(t, err)
+	t.Skip("As of 2018/9/7 this endpoints returns 404 and does not seem to work correctly")
+
+	cases := []*BranchesOption{
+		{},
+		{Limit: 1},
+		{SortBy: "id"},
+		{Offset: 0},
+	}
+
+	for i, opt := range cases {
+		branches, res, err := integrationClient.Branches.FindByRepositoryId(context.TODO(), integrationRepoId, opt)
+
+		if err != nil {
+			t.Fatalf("#%d unexpected error occured: %s", i, err)
+		}
+
+		if res.StatusCode != http.StatusOK {
+			t.Fatalf("#%d invalid http status: %s", i, res.Status)
+		}
+
+		if len(branches) == 0 {
+			t.Fatalf("#%d returned empty branches", i)
+		}
+
+		fmt.Println(branches)
+	}
+}
+
+func TestBranchesService_FindByRepositorySlug(t *testing.T) {
+	t.Parallel()
+
+	t.Skip("As of 2018/9/7 this endpoints returns 404 and does not seem to work correctly")
+
+	cases := []*BranchesOption{
+		{},
+		{Limit: 1},
+		{SortBy: "id"},
+		{Offset: 0},
+	}
+
+	for i, opt := range cases {
+		branches, res, err := integrationClient.Branches.FindByRepositorySlug(context.TODO(), integrationRepo, opt)
+
+		if err != nil {
+			t.Fatalf("#%d unexpected error occured: %s", i, err)
+		}
+
+		if res.StatusCode != http.StatusOK {
+			t.Fatalf("#%d invalid http status: %s", i, res.Status)
+		}
+
+		if len(branches) == 0 {
+			t.Fatalf("#%d returned empty branches", i)
+		}
+
+		fmt.Println(branches)
+	}
 }
