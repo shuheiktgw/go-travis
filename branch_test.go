8a50c20c74afa7de35ca032ac1e43cf4377f023b
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-09-11 10:07:05 +0900

    Add a unit test for BranchService FindByRepoSlug method

--- branch_test.go
+++ branch_test.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"net/url"
 	"reflect"
 	"testing"
 )
 
-const (
-	testRepoId = 12345
+var (
+	testRepoId   uint = 12345
+	testRepoSlug      = url.QueryEscape("shuheiktgw/go-travis-test")
 )
 
 func TestBranchService_FindByRepoId(t *testing.T) {
@@ -24,6 +26,29 @@ func TestBranchService_FindByRepoId(t *testing.T) {
 	})
 
 	branch, _, err := client.Branch.FindByRepoId(context.Background(), testRepoId, "master")
+
+	if err != nil {
+		t.Errorf("Branch.FindByRepoId returned error: %v", err)
+	}
+
+	want := &Branch{Name: "master", Repository: MinimalRepository{Id: 1, Name: "test", Slug: "shuheiktgw/test"}, DefaultBranch: true, ExistsOnGithub: true}
+	if !reflect.DeepEqual(branch, want) {
+		t.Errorf("Branch.FindByRepoId returned %+v, want %+v", branch, want)
+	}
+}
+
+func TestBranchService_FindByRepoSlug(t *testing.T) {
+	t.Parallel()
+
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc(fmt.Sprintf("/repo/%s/branch/%s", testRepoSlug, "master"), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"name":"master","repository":{"id":1,"name":"test","slug":"shuheiktgw/test"},"default_branch":true,"exists_on_github":true}`)
+	})
+
+	branch, _, err := client.Branch.FindByRepoSlug(context.Background(), testRepoSlug, "master")
 
 	if err != nil {
 		t.Errorf("Branch.FindByRepoId returned error: %v", err)
