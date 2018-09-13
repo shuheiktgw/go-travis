58ae421dcc749913f91c148232bac1efde8fdbdc
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-09-13 10:00:33 +0900

    Add unit tests for authentication endpoints

--- authentication_test.go
+++ authentication_test.go
@@ -3,34 +3,63 @@ package travis
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"reflect"
 	"testing"
 )
 
-func TestAuthenticate_UsingGithubToken_with_empty_token(t *testing.T) {
+func TestAuthenticationService_UsingGithubToken_without_token(t *testing.T) {
 	as := &AuthenticationService{client: NewClient(defaultBaseURL, "")}
 
 	_, _, err := as.UsingGithubToken(context.TODO(), "")
-	notOk(t, err)
+
+	if err == nil {
+		t.Fatal("AuthenticationService.UsingGithubToken with empty token: error is not supposed to be nil")
+	}
 }
 
-func TestAuthenticate_UsingTravisToken_with_empty_token(t *testing.T) {
+func TestAuthenticationService_UsingGithubToken_with_token(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/auth/github", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		testHeader(t, r, "Accept", "application/vnd.travis-ci.2.1+json")
+		fmt.Fprint(w, `{"access_token":"test_access_token"}`)
+	})
+
+	repo, _, err := client.Authentication.UsingGithubToken(context.Background(), "test_github_token")
+
+	if err != nil {
+		t.Errorf("AuthenticationService.UsingGithubToken returned error: %v", err)
+	}
+
+	want := AccessToken("test_access_token")
+	if !reflect.DeepEqual(repo, want) {
+		t.Errorf("AuthenticationService.UsingGithubToken returned %+v, want %+v", repo, want)
+	}
+}
+
+func TestAuthenticationService_UsingTravisToken_without_token(t *testing.T) {
 	as := &AuthenticationService{client: NewClient(defaultBaseURL, "")}
 
 	err := as.UsingTravisToken("")
-	notOk(t, err)
+	if err == nil {
+		t.Fatal("AuthenticationService.UsingTravisToken with empty token: error is not supposed to be nil")
+	}
 }
 
-func TestAuthenticate_UsingTravisToken_with_string_token(t *testing.T) {
+func TestAuthenticationService_UsingTravisToken_with_token(t *testing.T) {
 	token := "abc123easyasdoremi"
 	as := &AuthenticationService{client: NewClient(defaultBaseURL, token)}
 
 	err := as.UsingTravisToken(token)
-	ok(t, err)
+	if err != nil {
+		t.Fatalf("AuthenticationService.UsingTravisToken: unexpected error occurred: %s", err)
+	}
 
 	authHeader := as.client.Headers["Authorization"]
-	assert(
-		t,
-		authHeader == fmt.Sprintf("token %s", token),
-		fmt.Sprintf("travis token found in AuthenticationService.Headers: %s; expected %s", authHeader, token),
-	)
+	if authHeader != fmt.Sprintf("token %s", token) {
+		t.Fatalf("AuthenticationService.Headers: unexpected Authorization %s; expected %s", authHeader, token)
+	}
 }
