c70b48f39f0d23fed7c484c70a87f19b1dd761bd
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-09-15 10:45:55 +0900

    Update copyright statements

--- commit.go
+++ commit.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2015 Ableton AG, Berlin. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package travis
 
 // MinimalCommit is a minimal representation of a GitHub commit as seen by Travis CI
--- stage.go
+++ stage.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2015 Ableton AG, Berlin. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package travis
 
 // MinimalStage is a minimal representation of an individual stage
