8cf1db9fb420ca7a9bed4cd70c877879d97fa982
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-09-24 09:38:26 +0900

    Update bump.sh

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -4,7 +4,9 @@ echo current version: $(gobump show -r)
 read -p "input next version: " next_version
 
 gobump set $next_version -w
-git-chglog --next-tag v$next_version -o CHANGELOG.md
+git pull --tag
+git tag v$next_version
+git-chglog -o CHANGELOG.md
 
 git add version.go CHANGELOG.md
 git commit -m 'Bumps up to 'v$next_version
