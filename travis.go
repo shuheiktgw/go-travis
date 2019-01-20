c0b32566fe0a1d235f8a0edf63ae73a698feb611
Chris Broadfoot <cbro@golang.org>
2019-01-20 16:52:49 +0900

    Use HTTP body as error message if JSON decoding fails

--- travis.go
+++ travis.go
@@ -272,7 +272,9 @@ func checkResponse(r *http.Response) error {
 
 	data, err := ioutil.ReadAll(r.Body)
 	if err == nil && data != nil {
-		json.Unmarshal(data, errorResponse)
+		if err := json.Unmarshal(data, errorResponse); err != nil {
+			errorResponse.ErrorMessage = string(data)
+		}
 	}
 
 	return errorResponse
