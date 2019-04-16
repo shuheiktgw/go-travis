d4eb59b04d7375b4c2a19e38cfe536bb9a42f49a
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2019-04-16 10:28:08 +0900

    Extract Metadata struct into another file and add utility methods to judge its representation

---
+++ metadata.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2015 Ableton AG, Berlin. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package travis
+
+const (
+	standardRepresentation = "standard"
+	minimalRepresentation  = "minimal"
+)
+
+// Metadata is a metadata returned from the Travis CI API
+type Metadata struct {
+	// The type of data returned from the API
+	Type string `json:"@type,omitempty"`
+	// The link for data returned from the API
+	Href string `json:"@href,omitempty"`
+	// The representation of data returned from the API, standard or minimal
+	Representation string `json:"@representation,omitempty"`
+	// The permissions of data returned from the API
+	Permissions Permissions `json:"@permissions,omitempty"`
+}
+
+// IsStandard tells if the struct is in a standard representation
+func (m *Metadata) IsStandard() bool {
+	return m.Representation == standardRepresentation
+}
+
+// IsStandard tells if the struct is in a minimal representation
+func (m *Metadata) IsMinimal() bool {
+	return m.Representation == minimalRepresentation
+}
--- travis.go
+++ travis.go
@@ -304,17 +304,5 @@ func withContext(ctx context.Context, req *http.Request) *http.Request {
 	return req.WithContext(ctx)
 }
 
-// Metadata is a metadata returned from the Travis CI API
-type Metadata struct {
-	// The type of data returned from the API
-	Type string `json:"@type,omitempty"`
-	// The link for data returned from the API
-	Href string `json:"@href,omitempty"`
-	// The representation of data returned from the API, standard or minimal
-	Representation string `json:"@representation,omitempty"`
-	// The permissions of data returned from the API
-	Permissions Permissions `json:"@permissions,omitempty"`
-}
-
 // Permissions represents permissions of Travis CI API
 type Permissions map[string]bool
