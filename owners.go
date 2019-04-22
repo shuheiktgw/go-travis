6aff18bfc096d26be64941028f263b3ff4e48c95
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2019-04-22 10:28:51 +0900

    Add support for eager loading on the owner endpoint

--- owners.go
+++ owners.go
@@ -33,15 +33,25 @@ type Owner struct {
 	AvatarUrl string `json:"avatar_url"`
 	// Whether or not the owner has an education account
 	Education bool `json:"education"`
+	// Repositories belonging to this account
+	Repositories []*Repository `json:"repositories"`
+	// Installation belonging to the owner
+	Installation *Installation `json:"installation"`
 	*Metadata
+}
+
+// OwnerOption specifies the optional parameters for owner endpoint
+type OwnerOption struct {
+	// List of attributes to eager load
+	Include []string `url:"include,omitempty,comma"`
 }
 
 // Find fetches a owner based on the provided login
 // Login is user or organization login set on GitHub
 //
 // Travis CI API docs: https://developer.travis-ci.com/resource/owner#find
-func (os *OwnerService) FindByLogin(ctx context.Context, login string) (*Owner, *http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/owner/%s", login), nil)
+func (os *OwnerService) FindByLogin(ctx context.Context, login string, opt *OwnerOption) (*Owner, *http.Response, error) {
+	u, err := urlWithOptions(fmt.Sprintf("/owner/%s", login), opt)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -63,8 +73,8 @@ func (os *OwnerService) FindByLogin(ctx context.Context, login string) (*Owner, 
 // Find fetches a owner based on the provided GitHub id
 //
 // Travis CI API docs: https://developer.travis-ci.com/resource/owner#find
-func (os *OwnerService) FindByGitHubId(ctx context.Context, githubId uint) (*Owner, *http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/owner/github_id/%d", githubId), nil)
+func (os *OwnerService) FindByGitHubId(ctx context.Context, githubId uint, opt *OwnerOption) (*Owner, *http.Response, error) {
+	u, err := urlWithOptions(fmt.Sprintf("/owner/github_id/%d", githubId), opt)
 	if err != nil {
 		return nil, nil, err
 	}
