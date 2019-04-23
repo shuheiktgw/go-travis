3922a3c2df8db1b3981aec6f55c7185ce1deab48
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2019-04-23 10:41:22 +0900

    Add support for eager loadin on user endpoint

--- users.go
+++ users.go
@@ -37,14 +37,26 @@ type User struct {
 	IsSyncing bool `json:"is_syncing,omitempty"`
 	// The last time the user was synced with GitHub
 	SyncedAt string `json:"synced_at,omitempty"`
+	// Repositories belonging to this user
+	Repositories []*Repository `json:"repositories,omitempty"`
+	// Installation belonging to the user
+	Installation []*Repository `json:"installation,omitempty"`
+	// The user's emails
+	Emails []string `json:"emails,omitempty"`
 	*Metadata
+}
+
+// UserOption is query parameters to one can specify to find a user
+type UserOption struct {
+	// List of attributes to eager load
+	Include []string `url:"include,omitempty,comma"`
 }
 
 // Current fetches the currently authenticated user from Travis CI API.
 //
 // Travis CI API docs: https://developer.travis-ci.com/resource/user#current
-func (us *UserService) Current(ctx context.Context) (*User, *http.Response, error) {
-	u, err := urlWithOptions("/user", nil)
+func (us *UserService) Current(ctx context.Context, opt *UserOption) (*User, *http.Response, error) {
+	u, err := urlWithOptions("/user", opt)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -66,8 +78,8 @@ func (us *UserService) Current(ctx context.Context) (*User, *http.Response, erro
 // Get fetches the user with the provided id from the Travis CI API.
 //
 // Travis CI API docs: https://developer.travis-ci.com/resource/user#find
-func (us *UserService) Find(ctx context.Context, id uint) (*User, *http.Response, error) {
-	u, err := urlWithOptions(fmt.Sprintf("/user/%d", id), nil)
+func (us *UserService) Find(ctx context.Context, id uint, opt *UserOption) (*User, *http.Response, error) {
+	u, err := urlWithOptions(fmt.Sprintf("/user/%d", id), opt)
 	if err != nil {
 		return nil, nil, err
 	}
